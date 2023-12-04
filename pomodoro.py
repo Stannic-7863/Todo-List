64ffc18a3a1817699306f1c7a24c14fca5e026eb
Stannic <ayyan20218837@gmail.com>
2023-12-05 01:41:04 +0500

    new menu icon,clock font,finalized the pomoclock

--- pomodoro.py
+++ pomodoro.py
@@ -13,11 +13,12 @@ class Pomodoro(QWidget):
         self.pause_icon = QIcon('./data/icons/pause.png')
 
         #settings
-        self.total_minutes = 0.5
-        self.seconds =  0 
+        self.total_minutes = 25
+        self.elapsed_seconds =  0 
         self.total_seconds = self.total_minutes * 60
 
-        self.rounds = 4
+        self.total_rounds = 4
+        self.current_rounds = 0
 
         self.timer = QTimer()
         self.timer.setInterval(1000)
@@ -39,9 +40,20 @@ class Pomodoro(QWidget):
         self.add_task_button.setIcon(QIcon('./data/icons/plus.png'))
         self.add_task_button.setIconSize(QSize(50,50))
 
+        self.get_task_name = QLineEdit()
+        self.get_task_name.setMaximumWidth(0)
+        self.get_task_name.setStyleSheet(f"""QLineEdit {{
+                                         padding: 8px 8px 8px 8px;
+                                         border: none;
+                                         border-bottom: 1px solid white;
+                                         font-size: 20px;
+                                         }}""")  
+
         self.header_widget_layout.addWidget(self.options_button, alignment=Qt.AlignmentFlag.AlignLeft)
         self.header_widget_layout.addStretch()
+        self.header_widget_layout.addWidget(self.get_task_name, alignment=Qt.AlignmentFlag.AlignCenter)
         self.header_widget_layout.addWidget(self.add_task_button, alignment=Qt.AlignmentFlag.AlignCenter)
+        self.header_widget_layout.addSpacing(60)
         self.header_widget_layout.addStretch()
 
         self.body_widget = QWidget()
@@ -69,11 +81,33 @@ class Pomodoro(QWidget):
         self.set_button_style_sheet(self.options_button)
         self.set_button_style_sheet(self.add_task_button)
         self.set_button_style_sheet(self.start_pause_button)
+
+        self.add_task_button.pressed.connect(self.on_add_task_button_clicked)
         self.start_pause_button.pressed.connect(self.start_pause_pomodoro)
 
         self.main_layout = QVBoxLayout()
         self.main_layout.addWidget(self.container_widget)
         self.setLayout(self.main_layout)
+
+    def on_add_task_button_clicked(self):
+        self.animate_add_task_button()
+    
+    def animate_add_task_button(self):
+        self.animation_task_button = QPropertyAnimation(self.add_task_button, b'maximumWidth')
+        self.animation_task_button.setStartValue(self.add_task_button.width())
+        self.animation_task_button.setEndValue(0)
+        self.animation_task_button.setEasingCurve(QEasingCurve.Type.InCubic)
+        self.animation_task_button.finished.connect(self.animate_get_task)
+        self.animation_task_button.start()
+
+
+    def animate_get_task(self):
+        self.animation_get_task = QPropertyAnimation(self.get_task_name, b'minimumWidth')
+        self.animation_get_task.setStartValue(self.get_task_name.width())
+        self.animation_get_task.setEndValue(400)
+        self.animation_get_task.setEasingCurve(QEasingCurve.Type.OutBack)
+        self.animation_get_task.setDuration(500)
+        self.animation_get_task.start()
 
     def start_pause_pomodoro(self):
         if not self.timer.isActive():
@@ -84,14 +118,14 @@ class Pomodoro(QWidget):
             self.timer.stop()
 
     def update_clock_values(self):
-        self.seconds += 1 
-        time = self.get_formatted_time()
-        self.clock_widget.set_value(self.seconds, self.total_seconds, time)
+        self.elapsed_seconds += 1 
+        hour ,time = self.get_formatted_time()
+        self.clock_widget.set_value(self.elapsed_seconds, self.total_seconds, hour, time, self.total_rounds, self.current_rounds)
 
     def get_formatted_time(self):
-        minute, seconds = divmod(self.seconds, 60)
+        minute, elapsed_seconds = divmod(self.elapsed_seconds, 60)
         hour, minute = divmod(minute, 60)
-        return "{:02d}:{:02d}".format(minute, seconds)
+        return "{:02d}".format(hour) ,"{:02d}:{:02d}".format(minute, elapsed_seconds)
     
     def set_button_style_sheet(self, widget):
         widget.setStyleSheet(f"""QPushButton {{
@@ -103,8 +137,14 @@ class Pomodoro(QWidget):
 class CircularProgressBar(QWidget):
     def __init__(self):
         super().__init__()
+        font_path = "./data/fonts/clockfont.ttf" 
+        QFontDatabase.addApplicationFont(font_path)
+        self.clock_font = QFontDatabase.applicationFontFamilies(QFontDatabase.addApplicationFont(font_path))[0]
+        self.setFont(self.clock_font)
+
         self.value = 0
-        self.seconds = 0
+        self.elapsed_seconds = 0
+        self.hour = '00'
         self.time = '00:00'
         self.width = 300
         self.height = 300
@@ -113,7 +153,11 @@ class CircularProgressBar(QWidget):
         self.progress_color = priority_mid
         self.max_value = 100
         self.font_size = 30
+        self.font_size_small = 16
         self.enable_shadow = True
+        self.total_rounds = 4
+        self.current_rounds = 0
+        self.quote = 'Focus'
 
         self.resize(self.width, self.height)
         self.setMinimumSize(self.width, self.height)
@@ -128,9 +172,12 @@ class CircularProgressBar(QWidget):
         self.shadow.setColor(QColor(0,0,0,120))
         self.setGraphicsEffect(self.shadow)
 
-    def set_value(self, current_seconds, total_seconds, time):
-        self.value = (current_seconds/total_seconds)*100 
+    def set_value(self, elapsed_seconds, total_seconds, hour, time, total_rounds, current_rounds):
+        self.value = (elapsed_seconds/total_seconds)*100 
         self.time = time
+        self.hour = hour
+        self.current_rounds = current_rounds
+        self.total_rounds = total_rounds
         self.repaint()
 
     def paintEvent(self, event):
@@ -155,13 +202,38 @@ class CircularProgressBar(QWidget):
         
         pen.setColor(QColor(qRgb(self.progress_color[0], self.progress_color[1], self.progress_color[2])))
         pen.setWidth(self.progress_width)
+        
         if self.progress_rounded_cap:
             pen.setCapStyle(Qt.PenCapStyle.RoundCap)
         paint.setPen(pen)
         paint.drawArc(margin, margin, width, height, -90*16, -value*16)
 
+        pen.setColor(QColor(255,255,255,120))
+        paint.setPen(pen)
+        font = QFont()
+        font.setPointSize(self.font_size_small)
+        paint.setFont(font)
+        total_time_text_rect = QRect(margin, margin, width, height-self.font_size-self.progress_width-10-self.font_size_small)
+        paint.drawText(total_time_text_rect, Qt.AlignmentFlag.AlignCenter, f"{self.hour}:{self.time}")
 
         pen.setColor(QColor(qRgb(255,255,255)))
+        font = QFont()
+        font.setPointSize(self.font_size)
+        paint.setFont(font)
         paint.setPen(pen)
         paint.drawText(rect, Qt.AlignmentFlag.AlignCenter, f"{self.time}")
+
+        pen.setColor(QColor(255,255,255,120))
+        paint.setPen(pen)
+        font = QFont()
+        font.setPointSize(self.font_size_small)
+        paint.setFont(font)
+        percent_rounds_text_rect = QRect(margin, margin, width, height+self.font_size+self.progress_width+10+self.font_size_small)
+        paint.drawText(percent_rounds_text_rect, Qt.AlignmentFlag.AlignCenter, f"{int(self.value)}%    {self.current_rounds}/{self.total_rounds}")
+        
+        pen.setColor(QColor(255,255,255,255))
+        paint.setPen(pen)
+        quote_rect = QRect(margin, margin, width, height+height/2)
+        paint.drawText(quote_rect, Qt.AlignmentFlag.AlignCenter, f"{self.quote}")
+        
         paint.end() 
