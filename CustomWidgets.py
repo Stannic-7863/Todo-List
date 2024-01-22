9efa439558555697aa6aed99bb4150dffccf1dcb
Stannic <ayyan20218837@gmail.com>
2024-01-23 01:54:59 +0500

    Refactored, renamed functions and methods

--- CustomWidgets.py
+++ CustomWidgets.py
@@ -1,9 +1,8 @@
 from settings import *
-from Add_Task import Add_Task
-from PySide6.QtGui import QFont, QFontDatabase, QColor, qRgb, QShortcut, QKeySequence
+from TaskWidget import *
+from PySide6.QtGui import QColor, QShortcut, QKeySequence
 from PySide6.QtCore import Qt, QSize, QVariantAnimation, QAbstractAnimation
-from PySide6.QtWidgets import (QApplication,
-                            QWidget,
+from PySide6.QtWidgets import (QWidget,
                             QVBoxLayout,
                             QHBoxLayout,
                             QPushButton,
@@ -12,22 +11,33 @@ from PySide6.QtWidgets import (QApplication,
                             QRadioButton,
                             QButtonGroup,
                             QScrollBar,
-                            QSizePolicy
                             )
 
 import datetime
 from db_data_functions import commit_new_task_data
 
-class ItemLable(QLabel):
-    instances = []
-    def __init__(self, text, central, assigned):
+class NavMenuItemLabels(QLabel):
+    
+    activeInstance = None
+    
+    def __init__(self, name, parent, assigned):
         super().__init__()
-        self.instances.append(self)
-        self.setText(text)
+        self.setText(name)
         self.is_hovering = False
         self.selected = False
-        self.central = central
+        self.parent = parent
         self.assigned = assigned
+        
+        self.setStyleSheet(f"""
+            QLabel {{
+                font-size: 15px;
+                border-radius: 3px;
+                padding: 5px 2px 5px 2px;
+            }}
+            QLabel::hover {{
+                background-color : {accentdarkColor}
+            }}
+            """)
     
     def enterEvent(self, event):
         self.is_hovering = True
@@ -40,82 +50,84 @@ class ItemLable(QLabel):
             self.switch_widget()
 
     def switch_widget(self):
-        for instance in self.instances:
-                instance.selected = False
+        if NavMenuItemLabels.activeInstance and NavMenuItemLabels.activeInstance != self:
+            NavMenuItemLabels.activeInstance.selected = False
+            NavMenuItemLabels.activeInstance = self
+        else:
+            NavMenuItemLabels.activeInstance = None
+            
         self.selected = True
 
-        self.central.setCurrentWidget(self.assigned)
+        self.parent.stackedContainerLayout.setCurrentWidget(self.assigned)
 
-class Custom_Scroll_Bar(QScrollBar):
-    def __init__(self):
+class ScrollBar(QScrollBar):
+    def __init__(self) -> QScrollBar:
+        """
+        A customized QScrollBar
+        """
         super().__init__()
         self.setStyleSheet(f"""
-                            QScrollBar:vertical {{
-                            background: {backgroundColor};
-                            width: 20px;
-                            border: 0px solid black;
-                            margin: 15px 10px 15px 0px
-                            }}
+            QScrollBar:vertical {{
+            background: {backgroundColor};
+            width: 20px;
+            border: 0px solid black;
+            margin: 15px 10px 15px 0px
+            }}
 
-                            QScrollBar::handle:vertical {{
-                            border: 0px solid black;
-                            border-radius : 5px;
-                            background-color : {primaryColor}; 
-                            }}
+            QScrollBar::handle:vertical {{
+            border: 0px solid black;
+            border-radius : 5px;
+            background-color : {primaryColor}; 
+            }}
 
-                            QScrollBar::sub-line:vertical {{
-                            background: {backgroundColor};
-                            }}
-                            
-                            QScrollBar::add-line:vertical {{
-                            background: {backgroundColor}; 
-                            }}
+            QScrollBar::sub-line:vertical {{
+            background: {backgroundColor};
+            }}
+            
+            QScrollBar::add-line:vertical {{
+            background: {backgroundColor}; 
+            }}
 
-                            QScrollBar::sub-page:vertical {{
-                            background: {backgroundColor};
-                            }}
+            QScrollBar::sub-page:vertical {{
+            background: {backgroundColor};
+            }}
 
-                            QScrollBar::add-page:vertical {{
-                            background: {backgroundColor};
-                            }}
-                        """)
+            QScrollBar::add-page:vertical {{
+            background: {backgroundColor};
+            }}
+        """)
         
-
-    def enterEvent(self, event):
+    def enterEvent(self, event) -> bool:
         start_animation(self, primaryColor, priorityMidColor)
-
-    def leaveEvent(self, event):
+        return True
+    def leaveEvent(self, event) -> bool:
         start_animation(self, priorityMidColor, primaryColor)
-
+        return True
+    
 class GetTaskFromUser(QWidget):
-    def __init__(self, parent, mainwindowlayout):
+    def __init__(self, parent):
         super().__init__(parent)
         self.setMinimumWidth(200)
         self.parent = parent
-        self.mainwindowlayout = mainwindowlayout
         self.limit = 300
-        self.main_layout = QVBoxLayout()
-        self.setSizePolicy(QSizePolicy.Policy.Fixed, QSizePolicy.Policy.Fixed)
-
-        path = './data/fonts/bfont.TTF' 
-
-        fontinfo = QFontDatabase.applicationFontFamilies(QFontDatabase.addApplicationFont(path))
-        fontfamily = fontinfo[0] if fontinfo else 'Areil'
-        QApplication.setFont(QFont(fontfamily))
         
-        self.get_task_text_widget_layout = QVBoxLayout()
-        self.get_task_text_widget = QWidget()
-        self.get_task_text_widget.setLayout(self.get_task_text_widget_layout)
+        self.containerLayout = QVBoxLayout()
+        self.container = QWidget()
+        self.container.setLayout(self.containerLayout)
+        
+        self.mainlayout = QVBoxLayout()
+        self.setLayout(self.mainlayout)
+        self.mainlayout.addWidget(self.container)
+        
         self.getTaskName = QPlainTextEdit()
-        self.get_task_text_widget_layout.addWidget(self.getTaskName)
         self.getTaskName.setStyleSheet("max-height: 70")
         self.getTaskName.setVerticalScrollBarPolicy(Qt.ScrollBarPolicy.ScrollBarAlwaysOff)
-        self.getTaskName.textChanged.connect(self.word_limit)
-        self.getTaskName.setPlaceholderText('Enter Task Name and description')
+        self.getTaskName.textChanged.connect(self.LimitWordCount)
+        self.getTaskName.setPlaceholderText('Enter Task Name')
 
-        self.currentWordCountLabel = QLabel()
-        self.currentWordCountLabel.setText(f'{len(self.getTaskName.toPlainText())}/{self.limit}')
-        self.currentWordCountLabel.setStyleSheet(f"""QLabel {{
+        self.WordCountLabel = QLabel()
+        self.WordCountLabel.setText(f'{len(self.getTaskName.toPlainText())}/{self.limit}')
+        self.WordCountLabel.setStyleSheet(f"""QLabel {{
                             color : {backgroundColor};
                             font-weight: bold;}}
                         """)
@@ -138,45 +150,66 @@ class GetTaskFromUser(QWidget):
         self.set_radio_style_sheet(self.p_low, priorityLowColor)
         
         self.save_button = QPushButton('Save')
+        self.save_button.setFixedSize(QSize(70, 35))
+        
         cancel_shortcut = QShortcut(QKeySequence('escape'), self)
         cancel_shortcut.activated.connect(self.on_cancel)
         
-        self.save_button.setFixedSize(QSize(70, 35))
-    
-        self.main_layout.addWidget(self.get_task_text_widget)
         priority_button_container_layout.addWidget(priority_label, alignment=Qt.AlignmentFlag.AlignLeft)
         priority_button_container_layout.addWidget(self.p_high, alignment=Qt.AlignmentFlag.AlignLeft)
         priority_button_container_layout.addWidget(self.p_mid, alignment=Qt.AlignmentFlag.AlignLeft)
         priority_button_container_layout.addWidget(self.p_low, alignment=Qt.AlignmentFlag.AlignLeft)
         priority_button_container_layout.addStretch()
-        priority_button_container_layout.addWidget(self.currentWordCountLabel, alignment=Qt.AlignmentFlag.AlignRight)
-        self.main_layout.addWidget(priority_button_container)   
-        self.main_layout.addWidget(self.save_button, alignment=Qt.AlignmentFlag.AlignRight)
-
-        self.setLayout(self.main_layout)
+        priority_button_container_layout.addWidget(self.WordCountLabel, alignment=Qt.AlignmentFlag.AlignRight)
+        self.containerLayout.addWidget(self.getTaskName)
+        self.containerLayout.addWidget(priority_button_container)   
+        self.containerLayout.addWidget(self.save_button, alignment=Qt.AlignmentFlag.AlignRight)
 
         self.save_button.clicked.connect(self.on_save)
-        
 
         self.setStyleSheet(f"""
-                        QWidget {{border-radius: 5px}}
+                        QWidget {{
+                            background: {primaryColor};
+                            border-radius: 5px;
+                            max-width : 2000;
+                        }}
                         QPlainTextEdit {{
-                        background: {backgroundColor};
-                        border-radius: 5px
+                            background: {backgroundColor};
+                            border-radius: 5px
                         }}
                         QPushButton {{
-                        background: {backgroundColor};
-                        border-radius: 5px;
+                            background: {backgroundColor};
+                            border-radius: 5px;
                         }}
                         QRadioButton::indicator {{
-                        image : none;
+                            image : none;
                         }}
                         QLabel {{
-                        color : {fontColor}
+                            color : {fontColor}
                         }}
                         """)
 
-    def word_limit(self):
+    def on_save(self):
+        priority = 'none'
+        if self.p_high.isChecked():
+            priority = 'high'
+        if self.p_mid.isChecked():
+            priority = 'mid'
+        if self.p_low.isChecked():
+            priority = 'low'
+        
+        taskName = self.getTaskName.toPlainText().strip()
+        currentDatetime = datetime.datetime.now()
+        formattedDate = currentDatetime.strftime('%Y-%m-%d')
+        task_id = commit_new_task_data(taskName, str(formattedDate), priority, 'not done', None)
+        TaskCheckBox(self.parent ,taskName, priority, 'not done', task_id, False)
+
+        self.deleteLater()
+
+    def on_cancel(self):
+        self.deleteLater()
+        
+    def LimitWordCount(self):
         self.getTaskName.blockSignals(True)
         current_text = self.getTaskName.toPlainText()
         if len(current_text) > self.limit:
@@ -186,53 +219,29 @@ class GetTaskFromUser(QWidget):
             self.getTaskName.setCursor(cursor)
             
         currentWordCount = len(current_text)
-        self.currentWordCountLabel.setStyleSheet(f"""QLabel {{
+        self.WordCountLabel.setStyleSheet(f"""QLabel {{
                                                 color : {backgroundColor};
                                                 font-weight: bold;
                                                 }}""")
         if currentWordCount >= 300:
             currentWordCount = 300   
-            self.currentWordCountLabel.setStyleSheet(f"""QLabel {{
-                                                    color : rgb{str(priorityMidColor)};
+            self.WordCountLabel.setStyleSheet(f"""QLabel {{
+                                                    color : {priorityMidColor};
                                                     font-weight: bold;
                                                     }}""")
 
-        self.currentWordCountLabel.setText(f'{currentWordCount}/{self.limit}')
+        self.WordCountLabel.setText(f'{currentWordCount}/{self.limit}')
 
         self.getTaskName.blockSignals(False)
-
-    def on_save(self):
-        prio = 'none'
-        if self.p_high.isChecked():
-            prio = 'high'
-        if self.p_mid.isChecked():
-            prio = 'mid'
-        if self.p_low.isChecked():
-            prio = 'low'
-        
-        text = self.getTaskName.toPlainText().strip()
-        currentDatetime = datetime.datetime.now()
-        formattedDate = currentDatetime.strftime('%Y-%m-%d')
-        task_id = commit_new_task_data(text, str(formattedDate), prio, 'not done', None)
-        Add_Task(self.parent, self.mainwindowlayout ,text, prio, 'not done', task_id)
-
-        self.parent.on_task_added()
-        self.parent.placeholder_widget.deleteLater()
-        self.deleteLater()
-
-    def on_cancel(self):
-        self.parent.on_task_added()
-        self.parent.placeholder_widget.deleteLater()
-        self.deleteLater()
 
     def set_radio_style_sheet(self, widget, color):
         widget.setStyleSheet(f"""QRadioButton
                                 {{
-                                    background-color: rgb{color};
-                                    padding: 5px 10px 5px 0px;
+                                    background-color: {color};
+                                    padding: 5px 10px 5px 1px;
                                     text-align: center;
                                     width: 60px;
-                                    height: 15px;
+                                    height: 20px;
                                 }}
                                 QRadioButton:checked
                                 {{
